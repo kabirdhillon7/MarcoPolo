7b3889c71d2996898e9325681ba906d6705694fc
Kabir Dhillon <kabirdhillon99@gmail.com>
2022-02-06 15:13:46 -0800

    Updates

--- app/src/main/java/com/example/marcopolo/MainActivity.java
+++ app/src/main/java/com/example/marcopolo/MainActivity.java
@@ -1,18 +1,27 @@
 package com.example.marcopolo;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Button marcoButton;
         Button poloButton;
+        final String[] status = new String[1];
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -23,19 +32,40 @@ public class MainActivity extends AppCompatActivity {
         marcoButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this,
-                        R.string.marco_button_toast,
-                        Toast.LENGTH_SHORT).show();
+                status[0] = "Marco!";
+
+                // Start Activity
+                Intent intent = ScreenActivity.newIntent(MainActivity.this, status[0]);
+                startActivity(intent);
+
             }
         });
 
         poloButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this,
-                        R.string.polo_button_toast,
-                        Toast.LENGTH_SHORT).show();
+                status[0] = "Polo!";
+
+                // Start Activity
+                Intent intent = ScreenActivity.newIntent(MainActivity.this, status[0]);
+                startActivity(intent);
+
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == Activity.RESULT_OK){
+            Log.d(TAG, "Success");
+            return;
+        }
+        else{
+            Log.d(TAG, "Failure");
+            return;
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/marcopolo/ScreenActivity.java
@@ -0,0 +1,74 @@
+package com.example.marcopolo;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class ScreenActivity extends AppCompatActivity {
+    private String currentStatus;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.screen_activity);
+
+        TextView statusTextView;
+        statusTextView = findViewById(R.id.statusTextView);
+
+        currentStatus = getIntent().getStringExtra("Current Status");
+        statusTextView.setText(currentStatus);
+
+        if (currentStatus.equals("Marco!")){
+            setResult("Marco");
+        }
+        else if (currentStatus.equals("Polo!")){
+            setResult("Polo");
+        }
+    }
+
+    public void setResult(String postStatus) {
+        // Sending back data
+        Intent data = new Intent();
+        data.putExtra("Return Data", postStatus);
+        setResult(RESULT_OK, data);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // We're overriding our back button, and checking if it was clicked
+        if(keyCode == KeyEvent.KEYCODE_BACK){
+            /* To check what our current status is,
+            and based on that present the
+            correct toast.
+             */
+            currentStatus = getIntent().getStringExtra("Current Status");
+
+            if(currentStatus.equals("Marco!")){
+                Toast.makeText(ScreenActivity.this,
+                        R.string.polo_button_toast,
+                        Toast.LENGTH_SHORT).show();
+            }
+            else if(currentStatus.equals("Polo!")){
+                Toast.makeText(ScreenActivity.this,
+                        R.string.marco_button_toast,
+                        Toast.LENGTH_SHORT).show();
+            }
+
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    public static Intent newIntent(Context packageContext, String status){
+        Intent intent = new Intent(packageContext, ScreenActivity.class);
+        intent.putExtra("Current Status", status);
+
+        return intent;
+    }
+}
